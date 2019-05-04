4f702b5324106d84f9c55162e93c782d8aa67064
Pharap <2933055+Pharap@users.noreply.github.com>
2019-05-04 13:34:25 +0100

    Replace macros with constexpr

--- Source/Arduboy3D/Defines.h
+++ Source/Arduboy3D/Defines.h
@@ -15,38 +15,38 @@
 #endif
 
 #ifdef _WIN32
-#define DISPLAY_WIDTH 128
-#define DISPLAY_HEIGHT 64
+constexpr int DISPLAY_WIDTH { 128 };
+constexpr int DISPLAY_HEIGHT { 64 };
 #else
-#define DISPLAY_WIDTH 128
-#define DISPLAY_HEIGHT 64
+constexpr int DISPLAY_WIDTH { 128 };
+constexpr int DISPLAY_HEIGHT { 64 };
 #endif
 
-#define TARGET_FRAMERATE 30
+constexpr unsigned char TARGET_FRAMERATE { 30 };
 
-#define INPUT_LEFT 1
-#define INPUT_RIGHT 2
-#define INPUT_UP 4
-#define INPUT_DOWN 8
-#define INPUT_A 16
-#define INPUT_B 32
+constexpr unsigned char INPUT_LEFT { 1 << 0 };
+constexpr unsigned char INPUT_RIGHT { 1 << 1 };
+constexpr unsigned char INPUT_UP { 1 << 2 };
+constexpr unsigned char INPUT_DOWN { 1 << 3 };
+constexpr unsigned char INPUT_A { 1 << 4 };
+constexpr unsigned char INPUT_B { 1 << 5 };
 
-#define COLOUR_WHITE 1
-#define COLOUR_BLACK 0
+constexpr unsigned char COLOUR_WHITE { 1 };
+constexpr unsigned char COLOUR_BLACK { 0 };
 
-#define FIXED_ANGLE_MAX 256
+constexpr unsigned int FIXED_ANGLE_MAX { 256 };
 
-#define CAMERA_SCALE 1
-#define CLIP_PLANE 32
-#define CLIP_ANGLE 32
-#define NEAR_PLANE_MULTIPLIER 130
-#define NEAR_PLANE (DISPLAY_WIDTH * NEAR_PLANE_MULTIPLIER / 256)
-#define HORIZON (DISPLAY_HEIGHT / 2)
-#define MAP_SIZE 8
-#define CELL_SIZE 256
-#define PARTICLES_PER_SYSTEM 8
-#define BASE_SPRITE_SIZE 16
-#define MAX_SPRITE_SIZE (DISPLAY_HEIGHT / 2)
-#define MIN_TEXTURE_DISTANCE 4
+constexpr int CAMERA_SCALE { 1 };
+constexpr int CLIP_PLANE { 32 };
+constexpr int CLIP_ANGLE { 32 };
+constexpr int NEAR_PLANE_MULTIPLIER { 130 };
+constexpr int NEAR_PLANE { (DISPLAY_WIDTH * NEAR_PLANE_MULTIPLIER / 256) };
+constexpr int HORIZON { (DISPLAY_HEIGHT / 2) };
+constexpr int MAP_SIZE { 8 };
+constexpr int CELL_SIZE { 256 };
+constexpr int PARTICLES_PER_SYSTEM { 8 };
+constexpr int BASE_SPRITE_SIZE { 16 };
+constexpr int MAX_SPRITE_SIZE { (DISPLAY_HEIGHT / 2) };
+constexpr int MIN_TEXTURE_DISTANCE { 4 };
 
-#define TURN_SPEED 3
+constexpr int TURN_SPEED { 3 };
